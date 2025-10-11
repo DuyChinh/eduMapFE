9bb18534b3c73782fcee329e4c9771ac2cc40a60
DoanDuyChinh <doanchinhit21@gmail.com>
2025-10-11 17:11:40 +0700

    hot fix

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,20 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          antd: ['antd', '@ant-design/icons'],
+          router: ['react-router-dom'],
+          i18n: ['i18next', 'react-i18next'],
+          utils: ['axios', 'zustand']
+        }
+      }
+    }
+  },
+  server: {
+    historyApiFallback: true
+  }
 })
